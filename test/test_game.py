e6f77bfd9880d7fd40ba5628272454762c3d66d6
Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>
2021-10-05 09:15:40 -0300

    Test mark score

--- test/test_game.py
+++ test/test_game.py
@@ -3,7 +3,7 @@ sys.path.insert(0, './src')
 
 import pygame
 import unittest
-from game import Game
+from game import Game, Scoreboard
 
 class TestGame(unittest.TestCase):
     test_game: Game
@@ -40,6 +40,23 @@ class TestGame(unittest.TestCase):
         self.test_game.entities_dict['ball'].rect.bottom = 400
         self.assertTrue(self.test_game.check_goal())
         pygame.quit()
+
+
+class TestScoreboard(unittest.TestCase):
+    test_scoreboard: Scoreboard()
+
+    def test_mark_score(self):
+        self.test_scoreboard = Scoreboard()
+        
+        self.test_scoreboard.mark_score(None)
+        self.assertEqual(self.test_scoreboard.player_score['score'], 0)
+        self.assertEqual(self.test_scoreboard.enemy_score['score'], 0)
+
+        self.test_scoreboard.mark_score('player')
+        self.assertEqual(self.test_scoreboard.player_score['score'], 1)
+
+        self.test_scoreboard.mark_score('enemy')
+        self.assertEqual(self.test_scoreboard.enemy_score['score'], 1)
 
 
 if __name__ == '__main__':
